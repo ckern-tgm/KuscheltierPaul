ddbce5151de1db9f286199502ead2588e4bf5393
vschwartz-tgm <vschwartz@student.tgm.ac.at>
2018-10-17 15:35:23 +0200

    gitignore added

--- Website/functions.php
+++ Website/functions.php
@@ -1,15 +1,10 @@
 <?php
-function connect(){
-
-    //Establish username and password for bucket access
-    $authenticator = new \Couchbase\PasswordAuthenticator();
-    $authenticator ->username('uname')->password('pword');
-
-    //Connect to Couchbase Server
-    $cluster = new CouchbaseCluster("couchbase://127.0.0.1"); //placeholder string
-
-    //Authenticate
-    $cluster->authenticate($authenticator);
-    $bucket = $cluster->openBucket("medikamentenwecker");
-}
+/**
+ * @author Vincent Schwartz <schwartz@kuscheltier-paul.at>
+ * phpdoc
+ *
+ * @return void
+ *
+ *
+ */
 ?>
