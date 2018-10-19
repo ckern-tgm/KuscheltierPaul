d5fbcd38eb5424de4d47c7a37cbae4fb93d280e2
vschwartz-tgm <vschwartz@student.tgm.ac.at>
2018-10-19 11:24:17 +0200

    medikamente.html to .php

--- Website/functions.php
+++ Website/functions.php
@@ -1,5 +1,20 @@
 <?php
 /**
- * 
+ * Cool file.
+ *
+ * @author ich <ich@gmail.com>
+ *
+ * @version GIT: $Id$ In development. Very unstable.
  */
-?>
+
+/**
+ * Cool func.
+ *
+ * @return int
+ */
+function foo()
+{
+    $shit = 10;
+
+    return $shit;
+}
