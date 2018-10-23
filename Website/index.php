bece323e802da487d2f925bd2bc2ce655e7fc545
Paul Mazzolini <pmazzolini@student.tgm.ac.at>
2018-10-23 14:57:29 +0200

    Sitemap eingefügt und Video angepasst

--- Website/index.php
+++ Website/index.php
@@ -88,23 +88,31 @@
 			Das Kuscheltier lässt sich über die verbauten Knöpfe bedienen und über eine seniorenfreundliche Website steuern. Das Kuscheltier hat verschiedenste Funktionen um den Senioren den Alltag zu erleichtern, wie eine Medikamentenerinnerung oder einen Terminplaner. Aus Unterhaltungszwecken kann man mit dem Kuscheltier Simon says spielen und man kann sich Hörbücher anhören. Des Weiteren kann man seine Herzfrequenz messen.
 		</p>
 		
-		<!--
-		<video src="Video/Teaser.mp4" width="320"  height="240" controls poster="vorschaubild.jpg">
-			Dies Video kann in Ihrem Browser nicht wiedergegeben werden.<br> 
-		</video>-->
+		<br />
 		
+		<!--Video-->
 		<div class="embed-responsive embed-responsive-16by9" style="margin-left: 40px; margin-right: 40px;">
-			<iframe class="embed-responsive-item" src="Video/Teaser.mp4" allowfullscreen ></iframe>
+			<!--<iframe class="embed-responsive-item" src="Video/Teaser.mp4" allowfullscreen autoplay="0"></iframe>-->
+			<video src="Video/Teaser.mp4" controls poster="Bilder/poster.png">
+				Dies Video kann in Ihrem Browser nicht wiedergegeben werden.<br> 
+			</video>
 		</div>
 		
 		<br />
 		
 		<p style="background-color: white; margin-left: 40px; margin-right: 40px; text-align: justify; padding: 10px; font-size: 1.5vw;">
-			<u>Einzigartigkeit und Innovation?</u>
+			<u>Einzigartigkeit und Innovation:</u>
 			Geht man zunächst auf die Hinsicht der Unterhaltung ein, gibt es am Markt eine riesige Menge  an  Unterhaltungsgeräten.  Diese  sind  aber  Großteiles  sehr  teuer,  unhandlich und  für  die  ältere  Generation  schlichtweg  unverständlich.  Es  gibt  jedoch  bereits spezielle  Produkte  zur  Unterhaltung  von  Pensionisten,  wie  zum  Beispiel  einen Katzenroboter  der Firma  Hasbro  oder  einen  Hunderoboter.  Diese  dienen  aber ausschließlich zur Unterhaltung und Beschäftigung der Rentner.Roboter  für  Haushaltstätigkeiten  wie  Saugen,  Rasenmähen  oder Fensterputzen  gibt es  nun  seit  einigen  Jahren  und  sind  heute  weit  verbreitet.  Diese  Geräte  sind  aber oftmals   schwierig   zu   konfigurieren   und   bieten   eben   nur   in   ihren   jeweiligen Einsatzgebieten Hilfe. Aber auch in der Altenpflege wird Robotik bereits eingesetzt. Es gibt  einige  Roboter  und  Prototypen,  die  beispielsweise  in  Pflegeheimen  eingesetzt werden   und   mit   umfassender   Funktionalität   Pensionisten   unterstützen.   Die entscheidenden Probleme an diesen Assistenten sind, der große Platzbedarf und die sehr hohen Anschaffungskosten von mehreren Hunderttausenden bis Millionen Euro.Kaum ein Pensionist in Österreich könnte solche hohen Kosten tragen und somit bleibt der  Bedarf  eines  kompakten  und  preiswertem  interaktiven  Alltagshelfer  offen,  der sowohl    Unterhaltung    als    auch    Unterstützung    bietet.    Zusätzlich    fallen    die Verständnisprobleme mit der zuvor genannten Technik nicht weg. Hier bietet sich eine vertraute  Gestalt  wie  ein  Kuscheltier  an,  um  den  Pensionisten  die  Angst  vor  der Technik zu nehmen und ein Sicherheitsgefühl zu schaffen.
 		</p>
 		
 		<br />
+		
+		<p style="background-color: white; margin-left: 40px; margin-right: 40px; text-align: center; padding: 10px; font-size: 1.5vw;">
+			<u>Sitemap:</u> <br />
+			<br />
+			<img src="Bilder/Sitemap.png" alt="Sitemap" class="img-responsive center-block" />
+		</p>
+		
 		<br />
 		
 		<footer class="mainfooter" role="contentinfo">
