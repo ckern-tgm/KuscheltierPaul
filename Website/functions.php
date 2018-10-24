d58684c5f8a3b16b0961d7b4d87ed1ced451892e
vschwartz-tgm <vschwartz@student.tgm.ac.at>
2018-10-24 10:06:26 +0200

    addMedikamente

--- Website/functions.php
+++ Website/functions.php
@@ -8,13 +8,19 @@
  */
 
 /**
- * Cool func.
+ * Inserts a new row into table medikamentewecker.
  *
- * @return int
+ * @param $name, $mo, $di, $mi, $do, $fr, $sa, $so, $anz, $zeit
  */
 function addMedikament($name, $mo, $di, $mi, $do, $fr, $sa, $so, $anz, $zeit)
 {
     echo '<script language="javascript">';
     echo 'alert("addMedikament()")';
     echo '</script>';
+    /*
+    $dbconn = pg_connect('host=localhost port=5432 dbname=teddy user=vinc password=vinc');
+    pg_prepare($dbconn, 'addMedikament', 'INSERT INTO medikamentewecker VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)');
+    $insertValue = array($this->name, $this->mo, $this->di, $this->mi, $this->do, $this->fr, $this->sa, $this->so, $this->anz, $this->zeit);
+    pg_execute($dbconn, 'addMedikament', $insertValue);
+    */
 }
