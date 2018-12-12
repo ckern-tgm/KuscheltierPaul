cb281a3a32bc83b0c634d882e937c7ae0d8b6aee
vschwartz-tgm <vschwartz@student.tgm.ac.at>
2018-12-12 14:27:40 +0100

    passwortCheck

--- Website/passwortCheck.php
+++ Website/passwortCheck.php
@@ -2,14 +2,12 @@
 if (!session_id()) {
     session_start();
 }
+$input = $_POST["inputPwd"];
 
-$input = $_POST['input'];
-echo "<script>alert('<?php echo $input ?>')</script>";
-
-if (strcmp($input, $_SESSION["pwd"]) == 0) {
+if (strcmp($input, "Paul123") == 0) {
     $_SESSION["pwdChecked"] = "true";
     header('location:notfallsignal_change.php');
 } else {
-    echo "<script>alert('Passwort falsch')</script>";
+    //echo "<script>alert('Passwort falsch')</script>";
     header('location:notfallsignal_index.php');
 }
