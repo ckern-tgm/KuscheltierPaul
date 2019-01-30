4327a6d7de9f9330dbbc56fa13e06e96e07681d0
Paul Mazzolini <pmazzolini@student.tgm.ac.at>
2019-01-30 08:43:10 +0100

    Progressbars entfernt

--- Website/termine_neu.php
+++ Website/termine_neu.php
@@ -171,11 +171,13 @@ if (!session_id()) session_start();
 			<br />
 			<br />
 			
+			<!--
 			<div class="progress">
 				<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 25%;">
 					<p class="progress-bar-text">25%</p>
 				</div>
 			</div>
+			-->
 			
 			<br />
 			
